da496ceec97ea1720509b74580932be81569413c
Olof Ericsson <olof.ericsson@elev.ga.ntig.se>
2026-02-26 16:25:39 +0100

    Add user activities routes and views
    Add listing and creation UI and backend for user activities. Introduces GET /user/aktiviteter to query the aktiviteter table from databas.db and render views/aktiviteter.slim, GET /user/createaktivitet to render the creation form (views/skapa.slim), and POST /user/skapa to insert a new aktivitet and redirect back to the list. Adds a new db/aktiviteter.db file and updates databas.db. Leaves a stubbed post('/login') endpoint in app.rb.

--- app.rb
+++ app.rb
@@ -11,3 +11,33 @@ end
 post('/login') do
 
 end
+
+
+get('/user/aktiviteter') do
+
+  db = SQLite3::Database.new("databas.db")
+
+  db.results_as_hash = true
+
+  @aktiviteter = db.execute("SELECT * FROM aktiviteter")
+
+  p @aktiviteter
+
+  slim(:aktiviteter)
+end
+
+get('/user/createaktivitet') do
+  slim(:skapa)
+end
+
+post("/user/skapa") do 
+
+  new_aktivtet = params[:new_aktivitet]
+  description = params[:description]
+  
+  db = SQLite3::Database.new('databas.db')
+  db.execute("INSERT INTO aktiviteter (name, description) VALUES (?,?)",[new_aktivtet,description])
+  
+  redirect("/user/aktiviteter")
+
+end
