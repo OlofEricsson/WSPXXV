ae9973d74e2ea893a737f714db9415bf4e4056d6
Olof Ericsson <olof.ericsson@elev.ga.ntig.se>
2026-03-05 17:45:09 +0100

    Add edit/update/delete for aktiviteter
    Add CRUD endpoints and view for editing aktiviteter: implement GET /user/aktiviteter/:id/edit to load a single aktivitet, POST /user/aktiviteter/:id/update to update it, and POST /user/aktiviteter/:id/delete to remove it (app.rb). Add views/edit.slim with a prefilled edit form, and update views/aktiviteter.slim to point edit and delete actions at the new routes. The SQLite database file was also updated.

--- app.rb
+++ app.rb
@@ -34,10 +34,44 @@ post("/user/skapa") do
 
   new_aktivtet = params[:new_aktivitet]
   description = params[:description]
-  
+
   db = SQLite3::Database.new('databas.db')
   db.execute("INSERT INTO aktiviteter (name, description) VALUES (?,?)",[new_aktivtet,description])
-  
+
   redirect("/user/aktiviteter")
 
+end
+
+get("/user/aktiviteter/:id/edit") do
+
+  db = SQLite3::Database.new('databas.db')
+  db.results_as_hash = true
+  id = params[:id].to_i
+  @special_aktivitet = db.execute("SELECT * FROM aktiviteter WHERE id = ?", id).first
+
+  slim(:edit)
+
+end
+
+post("/user/aktiviteter/:id/update") do
+
+  id = params[:id].to_i
+  name = params[:name]
+  description = params[:description]
+
+  db = SQLite3::Database.new('databas.db')
+  db.execute("UPDATE aktiviteter SET name=?, description=? WHERE id=?",[name,description,id])
+
+  redirect("/user/aktiviteter")
+
+end
+
+post("/user/aktiviteter/:id/delete") do
+
+  id = params[:id].to_i
+  db = SQLite3::Database.new('databas.db')
+
+  db.execute("DELETE FROM aktiviteter WHERE id = ?", id)
+
+  redirect("/user/aktiviteter")
 end
