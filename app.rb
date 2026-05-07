db9c6658e4ce08822e023d2d5786a9b849ba4ec8
Olof Ericsson <olof.ericsson@elev.ga.ntig.se>
2026-05-07 12:49:53 +0200

    Revert "Merge branch 'main' of https://github.com/OlofEricsson/WSPXXV"
    This reverts commit 5f0d32f0a1235d5a3863c797062208fabd9ce78c, reversing
    changes made to 0af2c66f5ea2716887c441935ad862ebb5077dc6.

--- app.rb
+++ app.rb
@@ -4,8 +4,6 @@ require 'sqlite3'
 require 'sinatra/reloader'
 require 'bcrypt'
 require_relative 'model.rb'
-require_relative 'User.rb'
-require_relative 'Aktivitet.rb'
 
 include Model
 
@@ -20,9 +18,9 @@ enable :sessions
 before() do
 
   if (session[:user_id] !=  nil)
-    session[:user_info] = User.get_info_by_id(session[:user_id])
-    session[:username] = session[:user_info]["name"]
-
+    session[:user_info] = get_user_info_by_id(session[:user_id])
+    session[:username] = session[:user_info]['name']
+    
     @trainer_status = session[:user_info]['trainer']
     p @trainer_status
     if @trainer_status == 1
@@ -71,7 +69,8 @@ end
 post('/login') do
   username = params[:username]
   password = params[:password]
-  user = User.authenticate(username, password)
+
+  user = authenticate_user(username, password)
 
   if user
     p "Inloggad!"
@@ -92,7 +91,7 @@ end
 # @return [Slim Template] Renderar aktivitetsvyn.
 get('/user/aktiviteter') do
 
-  @aktiviteter = Aktivitet.activities_info()
+  @aktiviteter = aktiviteter_info()
 
   @trainer_status = session[:user_info]['trainer']
 
@@ -103,9 +102,9 @@ get('/user/aktiviteter') do
   @aktiviteter.each do |aktivitet|
     id = aktivitet["id"]
 
-    @kallade_per_aktivitet[id] = Aktivitet.get_called_names_by_id(id)
-    @frånvarande_per_aktivitet[id] = Aktivitet.get_abscents_names_by_id(id)
-    @kommer_per_aktivitet[id] = Aktivitet.get_coming_names_by_id(id)
+    @kallade_per_aktivitet[id] = get_called_names_by_id(id)
+    @frånvarande_per_aktivitet[id] = get_abscents_names_by_id(id)
+    @kommer_per_aktivitet[id] = get_coming_names_by_id(id)
   end
 
   slim(:aktiviteter)
@@ -126,7 +125,7 @@ end
 get("/user/aktiviteter/:id/edit") do
 
   id = params[:id].to_i
-  @special_aktivitet = Aktivitet.get_info_by_id(id)
+  @special_aktivitet = get_activity_info_by_id(id)
 
   slim(:edit)
 
@@ -137,7 +136,7 @@ end
 # @return [Slim Template] Renderar profilsidan.
 get('/user/profil') do
   username = session[:username]['name']
-  @user_info = User.get_active_info_by_name(username)
+  @user_info = get_active_info_by_name(username)
 
   slim(:profil)
 
@@ -160,7 +159,7 @@ post('/user/login/passwordchange') do
   username = params[:username]
   password = params[:password]
 
-  user = User.authenticate(username, password)
+  user = authenticate_user(username, password)
 
   if user
     p "Inloggad!"
@@ -197,7 +196,7 @@ post('/user/changepassword') do
   end
   hashed_password = BCrypt::Password.create(password)
 
-  User.change_password(hashed_password, session[:username])
+  change_password(hashed_password, session[:username])
 
   redirect("/user/profil")
 end
@@ -239,11 +238,11 @@ post("/userskapa") do
 
   hashed_password = BCrypt::Password.create(password)
 
-  User.create(username, hashed_password, trainer)
+  create_user(username, hashed_password, trainer)
 
   aktiv_id = db.last_insert_row_id
 
-  aktiviteter = Aktivitet.get_all_ids
+  aktiviteter = get_actvity_ids()
 
   aktiviteter.each do |aktivitet|
     update_relation(aktiv_id, aktivitet["id"])
@@ -263,7 +262,7 @@ post("/user/aktiviteter/:id/update") do
   name = params[:name]
   description = params[:description]
 
-  Aktivitet.update_by_id(name,description,id)
+  update_activity_by_id(name,description,id)
 
   redirect("/user/aktiviteter")
 
@@ -277,8 +276,8 @@ post("/user/aktiviteter/:id/delete") do
 
   id = params[:id].to_i
 
-  Aktivitet.delete_by_id(id)
-  
+  delete_activity_by_id(id)
+
   redirect("/user/aktiviteter")
 end
 
@@ -292,17 +291,17 @@ post('/user/createaktivitet') do
   name = params[:new_aktivitet]
   description = params[:description]
 
-  create_activity(name, description)
+  create_activity()
 
-  aktivitet_id = Aktivitet.last_insert_row_id
+  aktivitet_id = db.last_insert_row_id
 
-  aktiva = User.get_all_ids
+  aktiva = get_ids_from_actives()
 
   aktiva.each do |aktiv|
     update_relation(aktiv["id"], aktivitet_id)
   end
 
-  @kallade = User.get_called_names_by_id
+  @kallade = get_called_names_by_id(aktivitet_id)
 
   redirect("/user/aktiviteter")
 
